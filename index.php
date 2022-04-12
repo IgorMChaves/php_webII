0b11fb95e1d2d9ce853c5b0c0e1d2cc5f6574d3f
IgorMChaves <igor.1904.chaves@gmail.com>
2022-04-12 10:31:53 -0300

    aula 12/04/2022

--- index.php
+++ index.php
@@ -3,31 +3,48 @@
 spl_autoload_extensions('.php');
 function classLoader($class)
 {
+  $nomeArquivo = $class . '.php';
   $pastas = array('controller', 'model');
   foreach ($pastas as $pasta) {
-    $arquivo = "{$pasta}/{$class}.php";
+    $arquivo = "{$pasta}/{$nomeArquivo}";
     if (file_exists($arquivo)) {
       require_once($arquivo);
     }
   }
 }
-spl_autoload_register("classLoader");
-
-// front controller
-class Aplicacao{
-  public static function run(){
-    $layout = new Template("view/layout.html");
-
-    $class = "Inicio";
-    if(class_exists($class)){
+spl_autoload_register('classLoader');
+// Front Controller
+class Aplicacao
+{
+  private static $app = "/Igor";
+  public static function run()
+  {
+    $layout = new Template('view/layout.html');
+    if (isset($_GET["class"])) {
+      $class = $_GET["class"];
+    } else {
+      $class = "Inicio";
+    }
+    if (isset($_GET["method"])) {
+      $method = $_GET["method"];
+    } else {
+      $method = "";
+    }
+    if (class_exists($class)) {
       $pagina = new $class();
-      $conteudo = $pagina->controller();
-      $layout->set("conteudo", $conteudo);
-    }    
+      if (method_exists($pagina, $method)) {
+        $pagina->$method();
+      } else {
+        $pagina->controller();
+      }
+      $layout->set("uri", self::$app);
+      $layout->set('conteudo', $pagina->getMessage());
+    }
     echo $layout->saida();
   }
 }
 Aplicacao::run();
+
 
 
 
