a5135ced4564c39b1f3250d428fa3d58d9a85ab7
IgorMChaves <igor.1904.chaves@gmail.com>
2022-04-19 10:51:49 -0300

    aula 19/04/2022

--- controller/Form.php
+++ controller/Form.php
@@ -1,14 +1,38 @@
+<  
 <?php
 class Form
 {
   private $message = "";
+  public function __construct()
+  {
+    Transaction::open();
+  }
   public function controller()
   {
     $form = new Template("view/form.html");
     $this->message = $form->saida();
   }
+  public function salvar()
+  {
+    if (isset($_POST['paciente']) && isset($_POST['idade']) && isset($_POST['diagnostico'])) {
+      try {
+        $conexao = Transaction::get();
+        $consultorio = new Crud('consultorio');
+        $paciente = $conexao->quote($_POST['paciente']);
+        $idade = $conexao->quote($_POST['idade']);
+        $diagnostico = $conexao->quote($_POST['diagnostico']);
+        $resultado = $consultorio->insert("paciente, idade, diagnostico", "$paciente,$idade,$diagnostico");
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+  }
   public function getMessage()
   {
     return $this->message;
+  }
+  public function __destruct()
+  {
+    Transaction::close();
   }
 }
